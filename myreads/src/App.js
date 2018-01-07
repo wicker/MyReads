8f5342a5f2b2bc431604394c07a6b18b7e4e2da9
Jenner Hanni <jenner@wickerbox.net>
2018-01-07 13:32:08 -0800

    feature: Create page navigation.

--- myreads/src/App.js
+++ myreads/src/App.js
@@ -1,17 +1,52 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 
 class ListBooks extends React.Component {
   render() {
-    return 'List books'
+    return (
+			<div className="list-books">
+				<div className="list-books-title">
+					<h1>MyReads</h1>
+        </div>
+
+        <div className="list-books-content">
+          <div>
+            <Link className='open-search' to='/search'>Search</Link>
+					</div>
+				</div>
+      </div>
+		)
   }
 }
 
 class SearchBooks extends React.Component {
   render() {
-    return 'Search books'
+    return (
+
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link className="close-search" to="/">Close</Link>
+					<div className="search-books-input-wrapper">
+						{/*
+							NOTES: The search from BooksAPI is limited to a particular set of search terms.
+							You can find these search terms here:
+							https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+							However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+							you don't find a specific author or title. Every search is limited by search terms.
+						*/}
+						<input type="text" placeholder="Search by title or author"/>
+
+					</div>
+				</div>
+				<div className="search-books-results">
+					<ol className="books-grid"></ol>
+				</div>
+			</div>
+
+
+    )
   }
 }
 
