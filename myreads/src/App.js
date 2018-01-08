ccd025b4ad48d9d07203a9f0b6008caa94512d79
Jenner Hanni <jenner@wickerbox.net>
2018-01-08 08:38:01 -0800

    feature: Render books on the correct shelf

--- myreads/src/App.js
+++ myreads/src/App.js
@@ -13,7 +13,7 @@ class SelectShelf extends Component {
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
-          <option value="none">None</option>
+          <option value="none">Remove Book</option>
         </select>
       </div>
     )
@@ -23,7 +23,7 @@ class SelectShelf extends Component {
 class Book extends Component {
 
   static propTypes = {
-    book: PropTypes.array.isRequired
+    book: PropTypes.object.isRequired
   }
 
   render() {
@@ -31,22 +31,49 @@ class Book extends Component {
     const { book } = this.props
 
     return (
-        <div>
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: `${ book.width }`,
-                                                   height: `${ book.height }`,
-                                                   backgroundImage: `url(${ book.backgroundImage })` }}></div>
-              <SelectShelf />
-            </div>
-            <div className="book-title">{ book.title }</div>
-            <div className="book-authors">{ book.authors }</div>
+      <div key={ book.key }>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: `${ book.width }`,
+                                                 height: `${ book.height }`,
+                                                 backgroundImage: `url(${ book.backgroundImage })` }}></div>
+            <SelectShelf />
           </div>
+          <div className="book-title">{ book.title }</div>
+          <div className="book-authors">{ book.authors }</div>
         </div>
-      )
+      </div>
+    )
+  }
+}
 
+class BookShelf extends Component {
+
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    shelf: PropTypes.object.isRequired
   }
 
+  render() {
+
+    const { books, shelf } = this.props
+    var booksOnThisShelf
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{ shelf.title }</h2>
+        <div className="bookshelf-books">
+
+          {books.filter((book) =>
+            (book.shelf == shelf.funcName))
+                .map((book) =>
+            (<Book book={book}/>))
+          }
+
+        </div>
+      </div>
+    )
+  }
 }
 
 class ListBooks extends Component {
@@ -56,6 +83,13 @@ class ListBooks extends Component {
   }
 
   render() {
+
+    const shelves = [{ title: "Currently Reading",
+                       funcName: "currentlyReading" },
+                     { title: "Want To Read",
+                       funcName: "wantToRead" },
+                     { title: "Read",
+                       funcName: "alreadyFinished" }]
 
     const { books } = this.props
 
@@ -68,9 +102,10 @@ class ListBooks extends Component {
         <div className="list-books-content">
           <div>
 
-            {books.map((book) => (
-              <Book book={book}/>
-            ))}
+            <BookShelf books={books} shelf={shelves[0]}/>
+            <BookShelf books={books} shelf={shelves[1]}/>
+            <BookShelf books={books} shelf={shelves[2]}/>
+
           </div>
 
             <Link className='open-search' to='/search'>Search</Link>
@@ -104,7 +139,6 @@ class SearchBooks extends Component {
         </div>
       </div>
 
-
     )
   }
 }
@@ -114,18 +148,31 @@ class App extends Component {
   state = {
     books: [
       {
+        "key":1,
         "title": "To Kill a Mockingbird",
         "authors": "Harper Lee",
         "width": 128,
         "height": 193,
-        "backgroundImage": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+        "backgroundImage": "./icons/tokill.png",
+        "shelf":"currentlyReading",
       },
       {
+        "key":2,
         "title": "Ender's Game",
         "authors": "Orson Scott Card",
         "width": "128",
         "height": "188",
-        "backgroundImage": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
+        "backgroundImage": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
+        "shelf":"wantToRead",
+      },
+      {
+        "key":3,
+        "title": "Fine Prey",
+        "authors": "Scott Westerfeld",
+        "width": "128",
+        "height": "188",
+        "backgroundImage": "https://s17-us2.ixquick.com/cgi-bin/serveimage?url=https%3A%2F%2Fi.gr-assets.com%2Fimages%2FS%2Fcompressed.photo.goodreads.com%2Fbooks%2F1389237043i%2F186105._UY630_SR1200%2C630_.jpg&sp=784db9a71f3057c57adb4d40f34db473",
+        "shelf":"alreadyFinished",
       }
     ]
   }
