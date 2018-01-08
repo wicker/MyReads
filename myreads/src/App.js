4f91daa4d5c8a2449e05befcb7386202cebc6c41
Jenner Hanni <jenner@wickerbox.net>
2018-01-07 20:18:43 -0800

    style: Fix tabs to spaces

--- myreads/src/App.js
+++ myreads/src/App.js
@@ -7,16 +7,16 @@ class SelectShelf extends Component {
 
   render() {
     return (
-			<div className="book-shelf-changer">
-				<select>
-					<option value="none" disabled>Move to...</option>
-					<option value="currentlyReading">Currently Reading</option>
-					<option value="wantToRead">Want to Read</option>
-					<option value="read">Read</option>
-					<option value="none">None</option>
-				</select>
-			</div>
-		)
+      <div className="book-shelf-changer">
+        <select>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    )
   }
 }
 
@@ -32,17 +32,17 @@ class Book extends Component {
 
     return (
         <div>
-					<div className="book">
-						<div className="book-top">
-            	<div className="book-cover" style={{ width: `${ book.width }`,
-																									 height: `${ book.height }`,
-																									 backgroundImage: `url(${ book.backgroundImage })` }}></div>
-        	  	<SelectShelf />
-						</div>
-						<div className="book-title">{ book.title }</div>
-						<div className="book-authors">{ book.authors }</div>
-					</div>
-				</div>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: `${ book.width }`,
+                                                   height: `${ book.height }`,
+                                                   backgroundImage: `url(${ book.backgroundImage })` }}></div>
+              <SelectShelf />
+            </div>
+            <div className="book-title">{ book.title }</div>
+            <div className="book-authors">{ book.authors }</div>
+          </div>
+        </div>
       )
 
   }
@@ -60,9 +60,9 @@ class ListBooks extends Component {
     const { books } = this.props
 
     return (
-			<div className="list-books">
-				<div className="list-books-title">
-					<h1>MyReads</h1>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
         </div>
 
         <div className="list-books-content">
@@ -74,9 +74,9 @@ class ListBooks extends Component {
           </div>
 
             <Link className='open-search' to='/search'>Search</Link>
-				</div>
+        </div>
       </div>
-		)
+    )
   }
 }
 
@@ -84,25 +84,25 @@ class SearchBooks extends Component {
   render() {
     return (
 
-			<div className="search-books">
-				<div className="search-books-bar">
-					<Link className="close-search" to="/">Close</Link>
-					<div className="search-books-input-wrapper">
-						{/*
-							NOTES: The search from BooksAPI is limited to a particular set of search terms.
-							You can find these search terms here:
-							https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-							However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-							you don't find a specific author or title. Every search is limited by search terms.
-						*/}
-						<input type="text" placeholder="Search by title or author"/>
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input type="text" placeholder="Search by title or author"/>
 
-					</div>
-				</div>
-				<div className="search-books-results">
-					<ol className="books-grid"></ol>
-				</div>
-			</div>
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid"></ol>
+        </div>
+      </div>
 
 
     )
@@ -116,15 +116,15 @@ class App extends Component {
       {
         "title": "To Kill a Mockingbird",
         "authors": "Harper Lee",
-				"width": 128,
-				"height": 193,
-				"backgroundImage": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
+        "width": 128,
+        "height": 193,
+        "backgroundImage": "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api"
       },
       {
         "title": "Ender's Game",
         "authors": "Orson Scott Card",
-				"width": "128",
-   			"height": "188",
+        "width": "128",
+        "height": "188",
         "backgroundImage": "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api"
       }
     ]
