0fe04baafb7f5325d0e1ed4e18bef7027de64593
Jenner Hanni <jenner@wickerbox.net>
2018-01-08 15:24:32 -0800

    feature: Search form handles user inputs

--- myreads/src/App.js
+++ myreads/src/App.js
@@ -163,27 +163,23 @@ class SearchBooks extends Component {
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"
+
+            <input
+              type="text"
+              placeholder="Search by title or author"
               onChange={(event) => this.updateQuery(event.target.value)}
               value={query}
             />
-
 
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
 
-            {bookResults.map((book) =>
-              (<Book book={book} key={book.id} selectShelf={selectShelf}/>))
-            }
+            {bookResults && bookResults.length > 0
+                 ? bookResults.map((book) =>
+                   (<li key={book.id}><Book book={book} key={book.id} selectShelf={selectShelf}/></li>))
+                 : <li>No results</li> }
 
           </ol>
         </div>
